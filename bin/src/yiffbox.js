f75a9263145509868ad9864c6ab74a3edf43dacd
SamuraiStacks <samuraistacksmmlol@gmail.com>
2019-09-12 11:10:49 -0400

    fixed rating check

--- bin/src/yiffbox.js
+++ bin/src/yiffbox.js
@@ -1,8 +1,8 @@
 module.exports = (args) => {
     const ratings = ["safe", "questionable", "explicit"]
-
+    
     if(!args.rating) return console.log("A rating argument is required. It can be used like: \"--rating safe\".")
-    if(!args.rating.includes(ratings)) return console.log("Ratings either have to be safe, questionable or explicit.")
+    if(!ratings.includes(args.rating)) return console.log("Ratings either have to be safe, questionable or explicit.")
     
     if(!args.tags) return console.log("Atleast 1 tag is required. Usage: \"--tags fluffy\"")
     
