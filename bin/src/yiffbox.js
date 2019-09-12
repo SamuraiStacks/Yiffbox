35220a96108b5100b4442cf566add7332b469761
SamuraiStacks <samuraistacksmmlol@gmail.com>
2019-09-12 11:02:23 -0400

    added rating and tag check

--- bin/src/yiffbox.js
+++ bin/src/yiffbox.js
@@ -1,18 +1,10 @@
 module.exports = (args) => {
     const ratings = ["safe", "questionable", "explicit"]
+
     if(!args.rating) return console.log("A rating argument is required. It can be used like: \"--rating safe\".")
     if(!args.rating.includes(ratings)) return console.log("Ratings either have to be safe, questionable or explicit.")
+    
+    if(!args.tags) return console.log("Atleast 1 tag is required. Usage: \"--tags fluffy\"")
+    
     require("./downloader,js")(args.rating, args.tags)
-	/* switch(args.rating) {
-		case "safe":
-			require("./downloader.js")("safe", args.tags)
-			break;
-		case "explicit":
-			require("./downloader.js")("explicit", args.tags)
-			break;
-		case "questionable":
-			require("./downloader.js")("questionable", args.tags)
-			break;
-		default:
-			console.log("The rating has to be either safe, questionable or explicit.")      } */
 }
